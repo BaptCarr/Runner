091dd441230e9f3be03a01ba43c532e390878375
Baptiste CARROLAGGI <114500407+BaptCarr@users.noreply.github.com>
2022-10-17 19:10:17 +0200

    Create Camera.java

---
+++ src/Camera.java
@@ -0,0 +1,10 @@
+public class Camera {
+
+    private int x;
+    private int y;
+    private double ax, ay,k,f,m,vx,vy;
+
+    public Camera(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
