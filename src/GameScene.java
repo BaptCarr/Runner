7b00870c4d54c6a02b2757dbd77c9d94578f2b12
Baptiste CARROLAGGI <114500407+BaptCarr@users.noreply.github.com>
2022-10-17 19:08:36 +0200

    Update GameScene.java

--- src/GameScene.java
+++ src/GameScene.java
@@ -1,7 +1,11 @@
 import javax.swing.text.html.ImageView;
 import java.awt.geom.Rectangle2D;
 
-public class StaticThing {
-    private double SizeX,SizeY;
-    private ImageView vue;
-}
+public class GameScene {
+    private Camera cam;
+    private ImageView start;
+
+    public GameScene(Camera cam, ImageView start) {
+        this.cam = cam;
+        this.start = start;
+    }
