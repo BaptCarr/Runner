6280bcd02eeb11037b6fc84bdbba4d92ffa9995e
Baptiste CARROLAGGI <114500407+BaptCarr@users.noreply.github.com>
2022-11-21 11:39:07 +0100

    push

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 
 public class Main extends Application {
     /*
         *This is the main method
         *This a runner game 
     */
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello world");
+        Group root = new Group();
+        Pane pane = new Pane(root);
+        Gamescene theScene = new Gamescene(pane,800,400,true);
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(args);
     }
 }
